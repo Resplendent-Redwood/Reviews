adc656c5a4657d2fe7af871f989c0c44adb3ca22
Nancy Yang <nyyang.194@gmail.com>
2021-03-25 19:16:09 -0700

    Added join table for characteristic values.

--- database/index.js
+++ database/index.js
@@ -5,9 +5,34 @@ const connection = mysql.createConnection(mysqlConfig);
 
 connection.connect();
 
+const getReviewPhotos = function(reviewID) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT Photo_ID, Photo_URL FROM Reviews_Photos WHERE Review_ID=${reviewID}`, function (error, results) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
+
+
 const getProductRatings = function(id) {
   return new Promise((resolve, reject) => {
     connection.query(`SELECT Rating FROM Reviews WHERE Product_ID=${id}`, function (error, results) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
+
+const getProductRecommendations = function(id) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT Recommend FROM Reviews WHERE Product_ID=${id}`, function (error, results) {
       if (error) {
         reject(error);
       } else {
@@ -29,6 +54,56 @@ const getProductReviews = function(id) {
   });
 };
 
+const getProductCharacteristics = function(id) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT Characteristics_Name, Characteristics_ID FROM Characteristics WHERE Product_ID=${id}`, function (error, results) {
+      console.log('error:', error);
+      console.log('results:', results);
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
+
+const getCharacteristicsJoinTable = function(id) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT Characteristics.Characteristics_Name, Characteristics.Characteristics_ID, Characteristic_Reviews.Characteristic_Value FROM Characteristics INNER JOIN Characteristic_Reviews ON Characteristics.Characteristics_ID=Characteristic_Reviews.Characteristic_ID WHERE Product_ID=${id}`, function (error, results) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
+
+const getCharacterValue = function(id) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT Characteristic_Value FROM Characteristic_Reviews WHERE Characteristic_ID=${id}`, function (error, results) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
+
+
+
 module.exports.getProductRatings = getProductRatings;
 
 module.exports.getProductReviews = getProductReviews;
+
+module.exports.getProductRecommendations = getProductRecommendations;
+
+module.exports.getProductCharacteristics = getProductCharacteristics;
+
+module.exports.getCharacterValue = getCharacterValue;
+
+module.exports.getReviewPhotos = getReviewPhotos;
+
+module.exports.getCharacteristicsJoinTable = getCharacteristicsJoinTable;
--- index.js
+++ index.js
@@ -1,27 +1,90 @@
 const express = require('express');
 const db = require('./database');
-const port = 3000;
+const port = 3001;
 
 var app = express();
 
 app.use( express.json() );
 
-app.get('/ratings', function (req, res) {
-  const id = req.query.id;
-  db.getProductRatings(id).then((response) => {
-    let reviews = {};
-    for (let i = 0; i < response.length; i++) {
-      let rating = response[i]['Rating'];
-      reviews[rating] = 1;
-    }
-    res.send(reviews);
+app.get('/reviews', function (req, res) {
+  const id = req.query.product_id;
+  const sort = req.query.sort;
+  const count = req.query.count;
+  const page = req.query.page;
+  let reviewsObj = {
+    product: id,
+    page: page,
+    count: count,
+    results: null,
+    sort: sort
+  }
+
+  db.getProductReviews(id).then((response) => {
+    reviewsObj.results = response;
+    res.send(reviewsObj);
   }).catch((error) => console.log(error));
 });
 
-app.get('/reviews', function (req, res) {
-  const id = req.query.id;
-  db.getProductReviews(id).then((response) => {
-    res.send(response);
+app.get('/reviews/meta', function (req, res) {
+  const id = req.query.product_id;
+  let metaDataObject = {
+    product_id: id,
+  };
+
+  db.getProductRatings(id).then((response) => {
+    let ratingsObj = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0
+    };
+    for (let i = 0; i < response.length; i++) {
+      let rating = response[i]['Rating'];
+      if (!ratingsObj[rating]) {
+        ratingsObj[rating] = 1;
+      } else {
+        ratingsObj[rating]++;
+      }
+    }
+    metaDataObject.ratings = ratingsObj;
+  }).catch((error) => console.log(error));
+
+  db.getCharacteristicsJoinTable(id).then((response) => {
+    let characteristicsObj = {};
+
+    for (let i = 0; i < response.length; i++) {
+      let characterObj = {};
+      let charNumber = 1;
+      let name = response[i]['Characteristics_Name'];
+      let characteristicID = response[i]['Characteristics_ID'];
+      let characteristicValue = response[i]['Characteristic_Value'];
+
+      if (!characteristicsObj[name]) {
+        characterObj.id = characteristicID;
+        characterObj.value = characteristicValue;
+        characteristicsObj[name] = characterObj;
+        charNumber++;
+      };
+    }
+    metaDataObject.characteristics = characteristicsObj;
+  }).catch((error) => console.log(error));
+
+  db.getProductRecommendations(id).then((response) => {
+    let recommendedObj = {
+     true: 0,
+     false: 0
+    };
+    for (let i = 0; i < response.length; i++) {
+      let recommended = response[i]['Recommend'];
+      if (recommended === 1) {
+        recommendedObj.true++;
+      } else {
+        recommendedObj.false++;
+      }
+    }
+    metaDataObject.recommended = recommendedObj;
+    res.send(metaDataObject);
   }).catch((error) => console.log(error));
 });
 
